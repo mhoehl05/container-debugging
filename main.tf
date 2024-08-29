82f630a9506bd676e609ccc1175f6a859cf5fbcc
Höhl <MHOEHL@controlware.de>
2024-08-29 11:12:58 +0200

    basic kubernetes cluster

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
 data "azurerm_resource_group" "default" {
   name     = "rg-playground-test-weu"
 }
+
+resource "azurerm_kubernetes_cluster" "demo" {
+  name                = "aks-playground-test-weu"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2pls_v2"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
