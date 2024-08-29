e009270f9826399f330556950cb194bea7340996
Höhl <MHOEHL@controlware.de>
2024-08-29 13:56:09 +0200

    changed vmsize to B2ms

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "demo" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_B2ms"
   }
 
   identity {
